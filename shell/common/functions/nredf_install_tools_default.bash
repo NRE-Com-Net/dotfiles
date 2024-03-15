84beb77c926c6f1329363b9d367ce33a4861be70
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-03-15 16:09:08 +0100

    added lsd and some more fonts

--- shell/common/functions/nredf_install_tools_default.bash
+++ shell/common/functions/nredf_install_tools_default.bash
@@ -4,21 +4,22 @@
 
 function _nredf_install_tools_default() {
   echo -e "\033[1mLooking for fresh batteries\033[0m"
-  _nredf_tool_jq
-  _nredf_tool_fzf
-  _nredf_tool_helix
-  _nredf_tool_lf
-  _nredf_tool_lazygit
   _nredf_tool_btop
   _nredf_tool_ctop
-  _nredf_tool_zellij
-  _nredf_tool_ripgrep
-  _nredf_tool_fd
-  _nredf_tool_duf
   _nredf_tool_diskus
-  _nredf_tool_q
-  _nredf_tool_dust
-  _nredf_tool_yq
   [[ -f "${HOME}/.local/bin/drone" ]] && _nredf_tool_drone
+  _nredf_tool_duf
+  _nredf_tool_dust
+  _nredf_tool_fd
+  _nredf_tool_fzf
+  _nredf_tool_helix
+  _nredf_tool_jq
+  _nredf_tool_lazygit
+  _nredf_tool_lf
+  _nredf_tool_lsd
+  _nredf_tool_q
+  _nredf_tool_ripgrep
+  _nredf_tool_yq
+  _nredf_tool_zellij
   [[ -f "${HOME}/.kube/config" ]] && _nredf_install_tools_k8s_ops
 }
---
+++ shell/common/functions/nredf_tool_lsd.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+# shellcheck disable=SC2016,SC2155
+
+function _nredf_tool_lsd() {
+  _nredf_get_sys_info
+
+  local GHUSER="lsd-rs"
+  local GHREPO="lsd"
+  local BINARY="lsd"
+  local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
+  local VERSION="${TAGVERSION#v}"
+  local FILENAME="${BINARY}-${TAGVERSION}-${NREDF_UNAMEM}-${NREDF_PLATFORM}.tar.gz"
+  local VERSION_CMD="--version | awk '{print \$\2}'"
+  local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
+  local EXTRACT_CMD='
+    tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${XDG_BIN_HOME}/"
+  '
+
+  _nredf_install_tool "${BINARY}" "${FILENAME}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}"
+}
