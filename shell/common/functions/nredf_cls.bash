2a750961edca2c7d680514b3818d3a30f9af4f65
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-04-08 13:02:27 +0200

    adding clear screen comman with key bindings

---
+++ shell/common/functions/nredf_cls.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+
+function _nredf_cls() {
+  typeset -i COUNT LINES_BEFOREEND
+  COUNT=1
+  shopt -s checkwinsize 2>/dev/null
+  local LINES=${LINES:-(tput lines)}
+  local LINES_BEFOREEND=$(( LINES - 2 ))
+  while ((COUNT<=LINES_BEFOREEND)); do
+    echo ""
+    ((COUNT++))
+  done
+  printf '\33[%sA' "${LINES_BEFOREEND}"
+}
