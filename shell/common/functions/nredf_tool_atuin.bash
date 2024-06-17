49a40cba69ca2f57a42ac89200b06d94f134f373
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-06-17 10:43:37 +0200

    fixed atuin

--- shell/common/functions/nredf_tool_atuin.bash
+++ shell/common/functions/nredf_tool_atuin.bash
@@ -15,15 +15,15 @@ function _nredf_tool_atuin() {
   local BINARY="atuin"
   local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
   local VERSION="${TAGVERSION#v}"
-  local FILENAME="${BINARY}-${TAGVERSION}-${NREDF_UNAMEM}-${NREDF_PLATFORM}.tar.gz"
+  local FILENAME="${BINARY}-${NREDF_UNAMEM}-${NREDF_PLATFORM}.tar.gz"
   local VERSION_CMD="--version | awk '{print \$2}'"
   local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
   local EXTRACT_CMD='
     tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
     cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/${BINARY}" "${XDG_BIN_HOME}/"
-    cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/completions/_${BINARY}" "${XDG_CONFIG_HOME}/completion/zsh/_${BINARY}"
-    cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/completions/${BINARY}.bash" "${XDG_CONFIG_HOME}/completion/bash/${BINARY}.bash"
   '
 
   _nredf_install_tool "${BINARY}" "${FILENAME}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}" "${FORCE_INSTALL}"
+
+  _nredf_create_tool_completion "${BINARY}" "gen-completions --shell ${NREDF_SHELL_NAME}"
 }
