9fdbb856acedebbd8bdc95c6c8f546d1c5e652b4
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-06-28 13:58:20 +0200

    add github cli

---
+++ shell/common/functions/nredf_tool_github-cli.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+# shellcheck disable=SC2016,SC2155
+
+function _nredf_tool_github-cli() {
+  _nredf_get_sys_info
+
+  if [[ -n ${1} ]]; then
+    FORCE_INSTALL=true
+  fi
+
+  local GHUSER="cli"
+  local GHREPO="cli"
+  local BINARY="gh"
+  local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
+  local VERSION="${TAGVERSION#v}"
+  local FILENAME="${BINARY}_${VERSION}_${NREDF_UNAME_LOWER}_${NREDF_ARCH}.tar.gz"
+  local VERSION_CMD="--version | awk '{sub(/\+.*$/, "", \$3); print \$3}'"
+  local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
+  local EXTRACT_CMD='
+    tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
+    cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/bin/${BINARY}" "${XDG_BIN_HOME}/"
+  '
+
+  _nredf_install_tool "${BINARY}" "${FILENAME}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}" "${FORCE_INSTALL}"
+
+  _nredf_create_tool_completion "${BINARY}" "completion --shell ${NREDF_SHELL_NAME}"
+}
