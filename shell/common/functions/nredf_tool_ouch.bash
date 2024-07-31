c4bba78b8dff92c2fb859d2af324c39ccabd1794
root <root@server2.nrecom.net>
2024-07-31 13:00:20 +0200

    added ouch

---
+++ shell/common/functions/nredf_tool_ouch.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+# shellcheck disable=SC2016,SC2155
+
+function _nredf_tool_ouch() {
+  _nredf_get_sys_info
+
+  if [[ -n ${1} ]]; then
+    FORCE_INSTALL=true
+  fi
+
+  local GHUSER="ouch-org"
+  local GHREPO="ouch"
+  local BINARY="ouch"
+  local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
+  local VERSION="${TAGVERSION}"
+  local FILENAME="${BINARY}-${NREDF_UNAMEM}_${NREDF_PLATFORM}.tar.gz"
+  local VERSION_CMD="${XDG_BIN_HOME}/${BINARY} --version 2>/dev/null | awk '{print \$3}'"
+  local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
+  local EXTRACT_CMD='
+    command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
+    command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/${BINARY}" "${XDG_BIN_HOME}/"
+
+  '
+
+  _nredf_install_tool "${BINARY}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}" "${FORCE_INSTALL}"
+}
