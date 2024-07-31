4de889ff19cf4c7c69c23861a8399290e0d5028e
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-07-31 17:19:04 +0200

    use ouch

--- shell/common/functions/nredf_tool_ouch.bash
+++ shell/common/functions/nredf_tool_ouch.bash
@@ -15,13 +15,18 @@ function _nredf_tool_ouch() {
   local BINARY="ouch"
   local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
   local VERSION="${TAGVERSION}"
-  local FILENAME="${BINARY}-${NREDF_UNAMEM}_${NREDF_PLATFORM}.tar.gz"
-  local VERSION_CMD="${XDG_BIN_HOME}/${BINARY} --version 2>/dev/null | awk '{print \$3}'"
+  local FILENAME="${BINARY}-${NREDF_UNAMEM}-${NREDF_PLATFORM}.tar.gz"
+  local VERSION_CMD="${XDG_BIN_HOME}/${BINARY} --version 2>/dev/null | awk '{print \$2}'"
   local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
   local EXTRACT_CMD='
-    command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
+    if command -v ouch &> /dev/null; then
+      command ouch decompress -qy "${NREDF_DOWNLOADS}/${FILENAME}" -d "${NREDF_DOWNLOADS}/" 2>/dev/null
+    else
+      command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
+    fi
     command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/${BINARY}" "${XDG_BIN_HOME}/"
-
+    command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/completions/ouch.bash" "${XDG_CONFIG_HOME}/completion/bash/"
+    command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/completions/_ouch" "${XDG_CONFIG_HOME}/completion/zsh/"
   '
 
   _nredf_install_tool "${BINARY}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}" "${FORCE_INSTALL}"
