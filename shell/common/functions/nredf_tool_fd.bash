499236da693ea75751938aee440591133f0245eb
Steven Kurz <steven.koeberich@fhe3.com>
2024-08-27 10:18:59 +0200

    fixed... something

--- shell/common/functions/nredf_tool_fd.bash
+++ shell/common/functions/nredf_tool_fd.bash
@@ -21,7 +21,7 @@ function _nredf_tool_fd() {
   local VERSION="${TAGVERSION#v}"
   local FILENAME="${GHREPO}-${TAGVERSION}-${NREDF_UNAMEM}-${NREDF_PLATFORM}.tar.gz"
   local VERSION_CMD="${XDG_BIN_HOME}/${BINARY} --version | awk '/fd/{print \$2}'"
-  local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\"command "
+  local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
   local EXTRACT_CMD='
     command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
     command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/${BINARY}" "${XDG_BIN_HOME}/"
