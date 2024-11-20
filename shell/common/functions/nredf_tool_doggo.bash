7e10f3d7c24294e30ad52fe19c498a2f05071e86
Steven "NemesisRE" Kurz <nemesissre@gmail.com>
2024-11-20 15:37:10 +0100

    fixed doggo

--- shell/common/functions/nredf_tool_doggo.bash
+++ shell/common/functions/nredf_tool_doggo.bash
@@ -15,11 +15,12 @@ function _nredf_tool_doggo() {
   local BINARY="doggo"
   local TAGVERSION="${1:-$(_nredf_github_latest_release "${GHUSER}" "${GHREPO}")}"
   local VERSION="${TAGVERSION#v}"
-  local FILENAME="${GHREPO}_${VERSION}_${NREDF_UNAME_LOWER}_${NREDF_ARCH}.tar.gz"
+  local FILENAME="${GHREPO}_${VERSION}_${NREDF_UNAME}_${NREDF_UNAMEM}.tar.gz"
   local VERSION_CMD="${XDG_BIN_HOME}/${BINARY} --version | awk '{print \$1}'"
   local DOWNLOAD_CMD="_nredf_github_download_latest \"${GHUSER}\" \"${GHREPO}\" \"${FILENAME}\" \"${TAGVERSION}\""
   local EXTRACT_CMD='
-    command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${XDG_BIN_HOME}" "${BINARY}"
+    command tar -xzf "${NREDF_DOWNLOADS}/${FILENAME}" -C "${NREDF_DOWNLOADS}/"
+    command cp -f "${NREDF_DOWNLOADS}/${FILENAME%.tar.gz}/${BINARY}" "${XDG_BIN_HOME}/"
   '
 
   _nredf_install_tool "${BINARY}" "${TAGVERSION}" "${VERSION}" "${VERSION_CMD}" "${DOWNLOAD_CMD}" "${EXTRACT_CMD}" "${FORCE_INSTALL}"
